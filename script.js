0a2bf65fc6755e28e9934100232db4a429233d9c
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-08-29 18:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+
+function majortyNumber(arr,n) {
+	let count = 0;
+	for(let i of arr){
+
+		if(i == i+1){
+			count++;
+		}
+
+
+		if(count>Math.floor(n/2)){
+			return i;
+		}
+		
+		
+	}
+	return 0;
+}
