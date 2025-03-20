6398c7153718e8a7543ca25f57b02029dfabc5e0
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-03-20 16:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 //your code here
 
 function findMajorityElement(nums) {
-    let majorityElement = nums[0];
-    let count = 1;
 
-    for (let i = 1; i < nums.length; i++) {
-        if (count === 0) {
-            majorityElement = nums[i];
-            count = 1;
-        } else if (nums[i] === majorityElement) {
-            count++;
-        } else {
-            count--;
-        }
-    }
+	let map = new Map();
 
-    return majorityElement;
+	for(let i = 0 ; i< nums.length;i++){
+
+		if(map.has(nums[i])){
+			hm.set(nums[i],hm.get(nums[i],0)+1);
+		}
+		else{
+			hm.set(nums[i],1);
+		}
+
+		
+	}
+
+	for(let [key,value] in map){
+
+		if(value > 1){
+				return key;			
+		}
+	}
 }
